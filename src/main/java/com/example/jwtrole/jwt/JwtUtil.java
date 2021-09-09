59c608bd43efba51eb9e31fb7e72d64854c35e4b
mertcakmak2 <mertcakmak2@gmail.com>
2021-09-09 10:50:28 +0300

    for one day token

--- src/main/java/com/example/jwtrole/jwt/JwtUtil.java
+++ src/main/java/com/example/jwtrole/jwt/JwtUtil.java
@@ -7,10 +7,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 
 @Service
@@ -52,11 +49,13 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
+
         String token = Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                //.setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                .setExpiration(getForOneDayTokenDate())
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
         return token;
     }
@@ -64,5 +63,13 @@ public class JwtUtil {
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public Date getForOneDayTokenDate(){
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.MINUTE, 1440);
+        return c.getTime();
     }
 }
